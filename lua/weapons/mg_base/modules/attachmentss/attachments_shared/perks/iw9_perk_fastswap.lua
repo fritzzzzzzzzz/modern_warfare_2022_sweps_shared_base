3ef702ac74b8beb68681f509d9be4a27543ffa7f
fritzzzzzzzzz <aliasthedumpsterfire@gmail.com>
2025-09-28 14:54:59 +0800

    perkaholic

--- lua/weapons/mg_base/modules/attachmentss/attachments_shared/perks/iw9_perk_fastswap.lua
+++ lua/weapons/mg_base/modules/attachmentss/attachments_shared/perks/iw9_perk_fastswap.lua
@@ -5,6 +5,8 @@ ATTACHMENT.Icon = Material("easy/mw/attachments/icons/shared/jup_hud_icon_perks_
 local BaseClass = GetAttachmentBaseClass(ATTACHMENT.Base)
 function ATTACHMENT:Stats(weapon)
     BaseClass.Stats(self, weapon)
-    weapon.Animations.Draw = weapon.Animations.Draw_Quick
-    weapon.Animations.Holster = weapon.Animations.Holster_Quick
+    --weapon.Animations.Draw = weapon.Animations.Draw_Quick
+    --weapon.Animations.Holster = weapon.Animations.Holster_Quick
+    weapon.Animations.Draw.Fps = weapon.Animations.Draw.Fps * 1.5
+    weapon.Animations.Holster.Fps = weapon.Animations.Holster.Fps * 1.5
 end
--- lua/weapons/mg_base/modules/attachmentss/attachments_shared/perks/iw9_perk_melee.lua
+++ lua/weapons/mg_base/modules/attachmentss/attachments_shared/perks/iw9_perk_melee.lua
@@ -5,6 +5,7 @@ ATTACHMENT.Name = "Strong Arm"
 local BaseClass = GetAttachmentBaseClass(ATTACHMENT.Base)
 function ATTACHMENT:Stats(weapon)
     BaseClass.Stats(self, weapon)
-    weapon.Animations.Melee.Fps = weapon.Animations.Melee.Fps * 1.3
-    weapon.Animations.Melee_Hit.Fps = weapon.Animations.Melee_Hit.Fps * 1.2
+    weapon.Animations.Melee.Fps = weapon.Animations.Melee.Fps * 1.4
+    weapon.Animations.Melee_Hit.Fps = weapon.Animations.Melee_Hit.Fps * 1.3
+    weapon.Animations.Melee_Hit.Damage = weapon.Animations.Melee_Hit.Damage * 1.5
 end
